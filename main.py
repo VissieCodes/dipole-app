c4348c23e8a543657f714d7b497952137a4362ab
VissieCodes <vishishthaofficial@gmail.com>
2025-05-18 18:01:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,6 +72,10 @@ def register():
 
     except sqlite3.IntegrityError:
         return jsonify({"error": "Username or email already exists!"}), 409
+
+@app.route('/login', methods=['GET'])
+def login_page():
+    return render_template("login.html")
     
 @app.route('/login', methods=['POST'])
 def login():
